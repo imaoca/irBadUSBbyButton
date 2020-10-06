692352fe0f5c3e39fb2a4cd1e06455ed88a7bb2c
michihiro imaoka <imaoca@gmail.com>
2020-10-06 17:27:18 +0900

    Update BasUsbActivate.ino
    made gpio2 to vcc for IR receiver.

--- BasUsbActivate.ino
+++ BasUsbActivate.ino
@@ -1,11 +1,10 @@
-
 #include "Keyboard.h"
-#define Button5 9
 
 void setup() {
   Keyboard.begin();
   Serial1.begin(1200);
-  pinMode(Button5, INPUT_PULLUP);
+  pinMode(2,OUTPUT);
+  digitalWrite(2,HIGH);
 }
 
 void loop(){
@@ -15,18 +14,3 @@ void loop(){
     if (i=='B') Keyboard.println("BadUSB Activate attack plan B.");    
     if (i=='C') Keyboard.println("BadUSB Activate attack plan C.");
 }
-/*
-void loop() {
-  if(digitalRead(Button5) == LOW){
-    Keyboard.print("Arduino operating time ");
-    Keyboard.write(','); //
-    Keyboard.print(millis());
-    Keyboard.write('\n');
-    delay(100);
-
-    while(digitalRead(Button5) == LOW);
-  }
-
-  delay(100);
-}
-*/
